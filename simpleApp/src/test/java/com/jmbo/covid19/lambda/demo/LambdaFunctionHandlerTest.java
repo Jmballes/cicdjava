ae220d93899f7b0f14a49513ff99b76e3f3ba451
mi nombre <jmballes.ortega@gmail.com>
2020-04-14 22:14:35 +0200

    first comment

---
+++ simpleApp/src/test/java/com/jmbo/covid19/lambda/demo/LambdaFunctionHandlerTest.java
@@ -0,0 +1,69 @@
+package com.jmbo.covid19.lambda.demo;
+
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.jmbo.covid19.lambda.Codvid19SpainGetUpdates;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LambdaFunctionHandlerTest {
+
+    private final String CONTENT_TYPE = "image/jpeg";
+
+
+    @Mock
+    private AmazonS3 s3Client;
+    @Mock
+    private S3Object s3Object;
+
+    @Captor
+    private ArgumentCaptor<GetObjectRequest> getObjectRequest;
+
+    @Before
+    public void setUp() throws IOException {
+        
+
+
+    }
+
+    private Context createContext() {
+        TestContext ctx = new TestContext();
+
+        // TODO: customize your context here if needed.
+        ctx.setFunctionName("Your Function Name");
+
+        return ctx;
+    }
+
+    @Test
+    public void testLambdaFunctionHandler() {
+        Codvid19SpainGetUpdates handler = new Codvid19SpainGetUpdates(s3Client);
+        Context ctx = createContext();
+        Map<String, Object> input=new HashMap<String,Object>();
+        String output = handler.handleRequest(input, ctx);
+
+        // TODO: validate output here if needed.
+      //  Assert.assertEquals(CONTENT_TYPE, output);
+    }
+}
